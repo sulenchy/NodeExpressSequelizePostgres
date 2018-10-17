d03810e0a6412544ded82f8a8d853adfb73cd725
sulenchy <sulaimanabiodun172@gmail.com>
2018-10-17 21:55:54 +0100

    add sequelise

---
+++ server/config/connection.js
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
